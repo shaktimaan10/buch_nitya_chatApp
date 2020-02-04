f62527bbdcfb304cec2880ad5b963a42711ce3f6
shaktimaan10 <43183000+shaktimaan10@users.noreply.github.com>
2020-02-04 15:16:02 -0500

    Add files via upload
    voila

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// Importing Socket.io library
+const io = require('socket.io')();
+
 const port = process.env.PORT || 3030;
 
 // tell express where our static files are (js, images, css etc)
@@ -12,4 +15,29 @@ app.get('/', (req, res) => {
 
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
+});
+
+//This all is for Socket.io messaging functionality
+
+//attach socket.io
+io.attach(server);
+
+io.on('connection', function(socket) {
+    console.log("user is connected!");
+    socket.emit('connected', {sID: `${socket.id}`, message: 'new connection'});
+
+    socket.on('chat_message', function(msg){
+        console.log(msg);
+        
+        //when we get a message, we send it to everyone
+        io.emit('new_message', {id: socket.id, message: msg});
+    })
+
+    // listen for a disconnect event
+    socket.on('disconnect',function(){
+        console.log('a user is disconnected');
+
+        message = `${socket.id} has left the chat!`;
+        io.emit('user_disconnect', message);
+    })
 });
--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,1 +1,50 @@
 // imports always go first - if we're importing anything
+import ChatMessage from "./modules/ChatMessage.js";
+const socket = io();
+
+// Functions
+function sentUserId({sID, message}){
+    vm.socketID = sID;
+};
+
+function runDisconnectMessage(packet){
+    console.log(packet);
+};
+
+function appendNewMessage(msg){
+    vm.messages.push(msg);
+}
+
+//this is our main Vue instance
+const vm = new Vue({
+    data: {
+        socketID : "",
+        messages: [],
+        message: "",
+        nickName: ""
+    },
+
+    methods: {
+        dispatchMessage(){
+            // emit the message event and send a message to the sever
+            socket.emit('chat_message', {
+                content: this.message,
+                name: this.nickName || 'anonymous'
+            })
+            this.message = "";
+        }
+    },
+
+    components: {
+        newmessage: ChatMessage
+    },
+
+    mounted: function(){
+        console.log('mounted');
+    }
+}).$mount("#app");
+
+// some event handling -> these events are coming from the server
+socket.addEventListener('connected', sentUserId);
+socket.addEventListener('user_disconnect', runDisconnectMessage);
+socket.addEventListener('new_message', appendNewMessage);
